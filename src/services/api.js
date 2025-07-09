ef4ef545fdee3b2840757622732b2970634dc25b
PatgiriPP <140103819+PatgiriPP@users.noreply.github.com>
2025-07-10 00:45:41 +0530

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+const API_KEY = "c6535fd2c1d58f6e05b0595d47b01e08";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
+
